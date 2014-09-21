ab3af6ea8fa9059c3bf81245784401db4109343e
Robbie Gibson <rkgibson2@gmail.com>
2014-09-21 13:12:00 -0400

    added file that prints a string character by character

---
+++ section1/pstring.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    string name = "Robbie";
+
+    for (int i = 0, n = strlen(name); i < n; i++)
+    {
+	printf("%c\n", name[i]);
+    }
+}
