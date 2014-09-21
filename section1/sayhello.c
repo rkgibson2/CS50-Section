ba7c6fc920b4bc464337c352b790618d84aa5d01
Robbie Gibson <rkgibson2@gmail.com>
2014-09-21 13:20:38 -0400

    added sayhello

---
+++ section1/sayhello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(int argc, string argv[])
+{
+    printf("Please enter your name: ");
+    string name = GetString();
+    
+    // TODO
+    // this function isn't defined yet!!
+    sayhello(name);
+
+    return 0;
+}
