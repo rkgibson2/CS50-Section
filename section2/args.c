0adb83729dcc07e2ca18c7defe824cb54f6f9039
Robbie Kangjen Gibson <rkgibson2@gmail.com>
2015-09-20 22:43:43 +0000

    updated args.c to check argc

--- section2/args.c
+++ section2/args.c
@@ -3,9 +3,16 @@
 
 int main(int argc, string argv[])
 {
+
+    if (argc != 2)
+    {
+        printf("Improper usage\n");
+        return 1;
+    }
+
     int x = atoi(argv[1]);
-    
-    printf("%d\n", x);
-    
+
+    printf("%i\n", x);
+
     return 0;
 }
