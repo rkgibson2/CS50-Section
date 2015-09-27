6fb37766ff6477b140b23c1f9d318c2fe4ab08a5
Robbie Kangjen Gibson <rkgibson2@gmail.com>
2015-09-27 00:22:41 +0000

    added first draft of files for section 3

---
+++ section3/selection_sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+#define ARR_SIZE 5
+
+void sort(int array[], int n);
+
+int main(int argc, string argv[])
+{
+    int unsorted[ARR_SIZE] = {5, 2, 4, 3, 1};
+
+    // sort(unsorted, ARR_SIZE);
+
+    for (int i = 0; i < ARR_SIZE; i++)
+    {
+	printf("%d\t", unsorted[i]);
+    }
+
+    printf("\n");
+}
+
+/**
+ * Sorts array in place using selection sort
+ */
+void sort(int array[], int size)
+{
+    
+}
