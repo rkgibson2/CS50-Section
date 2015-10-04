42465a6d41c1d6730e880a8d6aa389a65a3ec633
Robbie Gibson <rkgibson2@gmail.com>
2015-10-04 03:53:01 +0000

    readded files from last year for section 4

---
+++ section4/std_IO.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    string input = GetString();
+    while (strcmp(input, "") != 0)
+    {
+        printf("%s\n", input);
+        input = GetString();
+    } 
+}
